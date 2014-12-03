78604ef48f64c04b5f7a9c5f7e64a8445733d811
Vincent <vincent.lapie@laposte.net>
2014-12-03 16:10:36 +0100

    Début des tests

---
+++ ServeurJMS/test/com/irc/server/ServerTest.java
@@ -0,0 +1,22 @@
+package com.irc.server;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ServerTest  extends TestCase {
+	@Before
+	public void setUp() {
+		
+	}
+
+	@After
+	public void tearDown() {
+
+	}
+
+	
+}
